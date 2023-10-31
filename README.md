d1a98f747b9505f9624a53effe6ee23ede87a50c
numiin <>
2023-11-01 00:16:30 +0400

    implementation

--- README.md
+++ README.md
@@ -1,1 +1,246 @@
-# argwork
+# argwork - shell command, completed
+
+`argwork` is a concept, an idea, that you can define a domain for your shell command in native terms and then let completion, usage and inspection work with no additional code required.
+
+
+## How it works
+
+In a prepared _Stack_ create a new _quasi command_:
+1. Put `_ <name>.sh` in any subdirectory of the stack root.
+2. Specify command inputs by defining your command domain.
+3. Put your action inside `main()`.
+
+Invoke the command using the runner with the completion built in.
+
+```
+[argwork-completion.bash]
+                          \
+                            [argwork quasi command]
+                          /
+    [argwork runner]
+```
+
+Basic outline for a _quasi command_:
+
+```bash
+at <...>
+...
+
+main() {
+  <your code>
+}
+```
+
+When _completion_ script is triggered the actual _quasi command_ script is `source`-d to provide implementation for `at` commands.
+That is why the actual command code is put into `main()` function, i.e. to protect it from being run by _completion_ script.
+
+`main()` will be invoked only by the `argwork` _runner_ executable script.
+
+### Parameter specification:
+
+```bash
+at  <index>  <name>  _       # Matches any input
+
+at  <index>  <name>  text     {eq | lt | gt | le | ge} <number>
+at  <index>  <name>  regex    <pattern>
+at  <index>  <name>  uuid
+at  <index>  <name>  date
+at  <index>  <name>  integer
+
+at  <index>  <name>  from     path/to/source
+at  <index>  <name>  opts     <option #1> {... <option #N>}
+at  <index>  <name>  command  <command> <arg 1> ...
+at  <index>  <name>  shell    <shell script>
+
+at  <index>  <name>  ...      # Dynamic continuation marker
+```
+
+`name` is a environment variable name that will be created and assigned a value corresponding to either positional of optional argument.
+
+`<index>` can be one of the following:
+* `1` `2` ... for **positional** parameter
+* `_` for an **optional** parameter
+
+### Positional parameters
+Those are positional in traditional understanding. There must be no gaps in numbering, although there is no requirement for a strict ordering (i.e. #2 can follow #4 as long as there is #3 specified at some point as well).
+
+### Optional parameters
+Those can be omitted, or specified only once, e.g.:
+
+```bash
+at  1  level   opts  WARN DEBUG
+at  _  target  opts  alpha beta other
+```
+
+It is possible to have a _quasi command_ with no positional **or** no optional parameters.
+
+The actual command may look like:
+
+```bash
+run DEBUG target: alpha
+```
+
+Sometimes it may be convenient to keep the optional name while omitting it as it were not specified at all.
+This may be achieved by specifying `_` in place of a value:
+
+```bash
+run WARN env: _
+```
+
+### Parameter ordering
+Positional arguments must immediately follow command name.
+
+Optional arguments must follow positional arguments.
+
+## Install
+
+### Manual installation
+
+* Put `argwork` executable on `PATH`
+* Source `argwork-completion.bash` in `.bashrc`
+
+Use symbolic links (`ln`) to make `git pull` automatically update with new changes.
+
+```bash
+ln -s argwork ~/.bin/argwork
+ln -s bash-completion.sh ~/.config/argwork/bash-completion.sh
+```
+
+Add to `.bashrc`:
+
+```bash
+source ~/.config/argwork/bash-completion.sh
+```
+
+## CLI
+
+* Positional arguments immediately follow the `<command> <quasi command>`
+* Optional arguments must follow positional and their value must be preceded by `<parameter name>:`
+* ` ?<TAB><TAB>` at the end of the command line will print `usage:` information
+* `??<TAB><TAB>` at the end of the command line will print `usage: (current)`, i.e. how arguments are bound to parameters
+
+Example _quasi command_ `random` in `draw` stack:
+```bash
+at 1 color   opts  red green yellow
+at 2 fruit   opts  papaya mango jackfruit durian
+at _ rating  integer
+
+main () {
+  echo "$color $fruit is rated ${rating:-friendly}"
+}
+```
+
+Command line may look something like:
+
+```bash
+$ draw random green ?<TAB><TAB>
+usage:  [1:color]  [2:fruit]  (rating)
+
+$ draw random yellow mango ??<TAB><TAB>
+usage (current):
+  1:color    = yellow
+  2:fruit    = mango
+  _:rating   =
+```
+
+## Setting up new command stack
+
+Related _quasi commands_ would likely share some assets or routines that make writing _quasi commands_ more convenient. Those _quasi commands_ can be organized by designating a certain root directory where all assets would be shared. This is called `argwork` _Stack_, while the directory is called `argwork` _Stack base_.
+
+It is recommended to use a semantic name for each new stack command so there would be no collision or accidental mix up of stacks.
+
+### Completion configuration
+
+Create a completion loader, e.g.:
+
+```bash
+_run_completion() {
+  ARGWORK_CLI_DIR="$HOME/path/to/stack/base"
+  _argwork_completion
+}
+
+complete -o nosort -F _run_completion run
+```
+
+Use one of the following options:
+
+* Create a new bash script with code similar to the following
+* Add or source the snippet in `.bashrc`
+* Put the file into `/usr/share/bash-completion/completions/`
+
+
+### Runner configuration
+
+Create a file names `run` on `PATH`, with the following content:
+
+```bash
+#!/bin/bash
+
+export ARGWORK_CLI_DIR="$HOME/path/to/stack/base"
+argwork "$@"
+```
+
+### Command stack framework
+
+_Stack base_ is defined by `ARGWORK_CLI_DIR` environment variable and tells the runner and completion where to lookup the assets:
+* `.env.sh` - contains a script that will be included before each _quasi command_ (i.e. global configuration)
+* Files whose content will be used for sourcing `from` parameter specification
+
+Also, _Stack base_ is required for making _quasi commands_ discoverable inside sub-directories (with names starting in `_ `).
+
+When you have a _quasi command_ and have a need for either `.env.sh` configuration or `from` parameter (i.e. sourcing possible argument values from a file) then you must have defined a root directory for performing the respective file lookup.
+
+### Naming convention
+* File with names `_ <name>.sh` will be interpreted as _quasi command_.
+* Directory with name `_ <name>` will be used to structure a path to a _quasi command_ and will be separated by `/` when building a command.
+
+The space after `_` is required.
+
+Names are case sensitive due to originating in a physical file system.
+
+### Example
+An example of a _Stack base_ that would model a database access tool set:
+
+```
+.env.sh
+_ cassandra
+  |
+    _ shell.sh
+    _ dsbulk
+      |
+        _ load.sh
+        _ unload.sh
+_ postgres
+  |
+    _ shell.sh
+keyspaces
+```
+
+After implementing `unload.sh` accordingly, you can export data from _Cassandra_ database by running the following command. It will be provided with completion for `keyspace` and `format` and will check the correctness of `table_name` format when actually run on command line:
+
+```bash
+run cassandra/dsbulk/unload table_name  format: csv
+```
+
+Note how `_ cassandra`/`_ dsbulk`/`_ unload.sh` physical path is expected in a more readable form `cassandra/dsbulk/unload`.
+
+Example of `unload.sh`:
+
+```bash
+#!/bin/bash
+
+at 1  keyspace    from   keyspaces
+at 2  table_name  regex  '^[a-zA-Z0-9_]*$'
+at _  format      opts   csv json
+
+main() {
+  dsbulk unload -k "$keyspace" -t "$table_name" > "$table_name.${format:-csv}"
+}
+```
+
+`keyspaces` text file content:
+
+```
+apples
+oranges
+```
---
+++ argwork-completion.bash
@@ -0,0 +1,308 @@
+#!/bin/bash
+
+__argwork_one() {
+  local index="$1"
+  local var="$2"
+  local type="$3"
+
+  if [[ "$index" != '_' && "$var" != '_' ]]; then
+    eval "$var='${COMP_WORDS[$(($index + 1))]}'"
+  fi
+
+  # update index
+  case "$index" in
+    _)
+      if [[ ! -v __ARGWORK_OPTIONAL_ARG_VARS[$var] ]]
+      then
+        __ARGWORK_OPTIONAL_PARAM_COUNT=$(($__ARGWORK_OPTIONAL_PARAM_COUNT + 1))
+        __ARGWORK_OPTIONAL_ARG_VARS["$var"]="$var"
+      fi
+      ;;
+    *)
+      if (($__ARGWORK_POSITIONAL_PARAM_COUNT < $index))
+      then
+        __ARGWORK_POSITIONAL_PARAM_COUNT=$index
+      fi
+      __ARGWORK_POSITIONAL_ARG_VARS[$index]="$var"
+      ;;
+  esac
+
+  if [[ "$index" == '_' ]]; then index="$var"; fi
+
+  case "$type" in
+    from)
+      __ARGWORK_LOOKUP_TYPES["$index"]='from'
+      __ARGWORK_LOOKUP_VALUES["$index"]="$4"
+      ;;
+    opts)
+      local _arg_list=(${@:4})
+      __ARGWORK_LOOKUP_TYPES["$index"]='opts'
+      __ARGWORK_LOOKUP_VALUES["$index"]=$(IFS=, ; echo "${_arg_list[*]}")
+      ;;
+    shell)
+      RUN_SHELL_ARGS=(${COMP_WORDS[@]:1})
+      __ARGWORK_LOOKUP_TYPES["$index"]='shell'
+      __ARGWORK_LOOKUP_VALUES["$index"]="$4"
+      ;;
+    command)
+      __ARGWORK_LOOKUP_TYPES["$index"]='command'
+      __ARGWORK_LOOKUP_VALUES["$index"]="$4"
+      ;;
+    uuid)
+      __ARGWORK_LOOKUP_TYPES["$index"]='uuid'
+      __ARGWORK_LOOKUP_VALUES["$index"]="[UUID]"
+      ;;
+    date)
+      __ARGWORK_LOOKUP_TYPES["$index"]='date'
+      __ARGWORK_LOOKUP_VALUES["$index"]='[YYYY-MM-DD]'
+      ;;
+    text)
+      __ARGWORK_LOOKUP_TYPES["$index"]='text'
+      __ARGWORK_LOOKUP_VALUES["$index"]='[TEXT]'
+      ;;
+    regex)
+      __ARGWORK_LOOKUP_TYPES["$index"]='regex'
+      __ARGWORK_LOOKUP_VALUES["$index"]='[REGEX]'
+      ;;
+    integer)
+      __ARGWORK_LOOKUP_TYPES["$index"]='integer'
+      __ARGWORK_LOOKUP_VALUES["$index"]='[INTEGER]'
+      ;;
+    _)
+      __ARGWORK_LOOKUP_TYPES["$index"]='_'
+      __ARGWORK_LOOKUP_VALUES["$index"]=
+      ;;
+    ...)
+      __ARGWORK_LOOKUP_TYPES["$index"]=
+      __ARGWORK_LOOKUP_VALUES["$index"]=
+      ;;
+    *)
+      ;;
+  esac
+
+}
+
+__argwork_script_name_to_path() {
+  IFS='/' read -r -a __script_name_sections <<< "$1"
+  local section_array=("${__script_name_sections[@]/#/_ }")
+  printf '/%s' "${section_array[@]}"
+}
+
+__argwork_complete() {
+  # Optionals consist of three parts and are specifired like <param>:<value>
+  if [[ ${COMP_CWORD} -gt $(($__ARGWORK_POSITIONAL_PARAM_COUNT + $__ARGWORK_OPTIONAL_PARAM_COUNT * 3 + 1)) ]]; then return; fi
+
+  local sector=POSITIONAL
+  if (( $__ARGWORK_POSITIONAL_PARAM_COUNT > 0 && ${COMP_CWORD} > $__ARGWORK_POSITIONAL_PARAM_COUNT + 1))
+  then
+    sector=OPTIONAL
+  fi
+
+  local key=
+  local key_shift=0
+  case $sector in
+    POSITIONAL)
+      key="$(($COMP_CWORD - 1))"
+      ;;
+    OPTIONAL)
+      if [[ "${COMP_WORDS[$COMP_CWORD]}" == ':' ]]
+      then
+        key="${COMP_WORDS[$(($COMP_CWORD - 1))]}"
+        key_shift=1
+        if [[ -z "$key" || ! -v __ARGWORK_OPTIONAL_ARG_VARS[$key] ]]; then return; fi
+      elif [[ "${COMP_WORDS[$(($COMP_CWORD - 1))]}" == ':' ]]
+      then
+        key="${COMP_WORDS[$(($COMP_CWORD - 2))]}"
+        key_shift=0
+        if [[ -z "$key" || ! -v __ARGWORK_OPTIONAL_ARG_VARS[$key] ]]; then return; fi
+      else
+        COMPREPLY=($(compgen -W "$(printf "%s\n" "${__ARGWORK_OPTIONAL_ARG_VARS[@]/%/:}")" -- "${COMP_WORDS[$COMP_CWORD]}"))
+        return
+      fi
+      ;;
+  esac
+
+  local word_index=$(($COMP_CWORD + $key_shift))
+
+  # echo "==> DEBUG: __ARGWORK_POSITIONAL_PARAM_COUNT=$__ARGWORK_POSITIONAL_PARAM_COUNT ; __ARGWORK_OPTIONAL_PARAM_COUNT=$__ARGWORK_OPTIONAL_PARAM_COUNT ; __ARGWORK_LOOKUP_TYPES=$!__ARGWORK_LOOKUP_TYPES[@]} ; key=$key ; __ARGWORK_LOOKUP_TYPES[key]=${__ARGWORK_LOOKUP_TYPES[$key]} ; __ARGWORK_POSITIONAL_ARG_VARS=[${__ARGWORK_POSITIONAL_ARG_VARS[@]}]" >> ~/argwork-completion.bash.log
+
+  case "${__ARGWORK_LOOKUP_TYPES[$key]}" in
+    from)
+      if [[ -f "${ARGWORK_CLI_DIR}/${__ARGWORK_LOOKUP_VALUES[$key]}" ]]; then
+        IFS=$'\n' COMPREPLY=($(compgen -W "$(cat "${ARGWORK_CLI_DIR}/${__ARGWORK_LOOKUP_VALUES[$key]}")" -- "${COMP_WORDS[$word_index]}"))
+      fi
+      ;;
+    opts)
+      split_into_lines="$(echo "${__ARGWORK_LOOKUP_VALUES[$key]}" | sed 's/,/\n/g')"
+      IFS=$'\n' opts_split=($(echo "$split_into_lines"))
+
+      IFS=$'\n' COMPREPLY=($(compgen -W "$(printf "%s\n" "${opts_split[@]}")" -- "${COMP_WORDS[$word_index]}"))
+      ;;
+    shell)
+      shell_code="${__ARGWORK_LOOKUP_VALUES[$key]}"
+      IFS=$'\n' COMPREPLY=($(compgen -W "$(printf "%s\n" "$(eval "$shell_code")")" -- "${COMP_WORDS[$word_index]}"))
+      ;;
+    command)
+      IFS=' ' command_line="${__ARGWORK_LOOKUP_VALUES[$key]} ${COMP_WORDS[*]:2}"
+      IFS=$'\n' COMPREPLY=($(compgen -W "$(printf "%s\n" "$(eval "$command_line")")" -- "${COMP_WORDS[$word_index]}"))
+      ;;
+    uuid | date | text | integer)
+      IFS=$'\n' COMPREPLY=($(compgen -W "${__ARGWORK_LOOKUP_VALUES[$key]}" -- "${COMP_WORDS[$word_index]}"))
+      ;;
+    _)
+      IFS=$'\n' COMPREPLY=($(compgen -W '_' -- "${COMP_WORDS[$word_index]}"))
+      ;;
+    *)
+      ;;
+  esac
+}
+
+__argwork_complete_help() {
+  local help_spec=''
+
+  for key in $(seq 1 $__ARGWORK_POSITIONAL_PARAM_COUNT)
+  do
+    local spec=
+    local var_name="${__ARGWORK_POSITIONAL_ARG_VARS[$key]}"
+    if [[ "$var_name" == '_' ]]; then
+      spec='...'
+    else
+      spec="$key:$var_name"
+    fi
+    help_spec="$help_spec  [$spec]"
+  done
+  local optional_arg_spec="${__ARGWORK_OPTIONAL_ARG_VARS[@]}"
+  help_spec="$help_spec  ${optional_arg_spec:+($optional_arg_spec)}"
+  COMPREPLY=("usage:$help_spec" '')
+}
+
+__argwork_complete_inspect() {
+  declare -A arg_map
+  local sector=POSITIONAL
+  local option_name=
+
+  for index in $(seq 2 "$((${#COMP_WORDS[@]} - 2))")
+  do
+    local word="${COMP_WORDS[$index]}"
+    local word_1="${COMP_WORDS[$(($index + 1))]}"
+
+    if [[ "$word" == ':' ]]
+    then
+      continue
+    elif [[ "$word" != ':' && "$word_1" == ':' ]]
+    then
+      sector=OPTIONAL
+      if [[ ! -z "$option_name" ]]; then
+        return
+      fi
+      option_name="$word"
+    else
+      case $sector in
+        OPTIONAL)
+          if [[ -z "$option_name" ]]; then
+            return
+          fi
+          case "$word" in
+            _)
+              arg_map["$option_name"]=
+              ;;
+            *)
+              arg_map["$option_name"]="$word"
+              ;;
+          esac
+          option_name=
+          ;;
+        POSITIONAL)
+          arg_map["$(($index - 1))"]="$word"
+          ;;
+      esac
+    fi
+  done
+  local help_spec=''
+
+  printf '\nusage (current):'
+
+  for key in $(seq 1 $__ARGWORK_POSITIONAL_PARAM_COUNT)
+  do
+    local field="${key}:${__ARGWORK_POSITIONAL_ARG_VARS[$key]}"
+    printf '\n  %s%*s = %s' "$field" "$((20-${#field}))" '' "${arg_map[$key]}"
+  done
+  for option_name in "${!__ARGWORK_OPTIONAL_ARG_VARS[@]}"
+  do
+    printf '\n  %s%*s = %s' "_:$option_name" "$((18-${#option_name}))" '' "${arg_map[$option_name]}"
+  done
+  COMPREPLY=('')
+}
+
+_argwork_completion() {
+  declare -A __ARGWORK_LOOKUP_TYPES
+  declare -A __ARGWORK_LOOKUP_VALUES
+  declare -a __ARGWORK_POSITIONAL_ARG_VARS
+  declare -A __ARGWORK_OPTIONAL_ARG_VARS
+  declare -a __ARGWORK_CURRENT_VALS
+
+  __ARGWORK_POSITIONAL_PARAM_COUNT=0
+  __ARGWORK_OPTIONAL_PARAM_COUNT=0
+  __ARGWORK_POSITIONAL_ARG_VARS[0]=
+  __ARGWORK_CURRENT_VALS[0]=
+
+  __ARGWORK_SCRIPT_PATH="$(__argwork_script_name_to_path "${COMP_WORDS[1]}")"
+
+  if [ "${COMP_CWORD}" -gt 1 ] && [ ! -f "$ARGWORK_CLI_DIR/${__ARGWORK_SCRIPT_PATH}.sh" ]
+  then
+    COMPREPLY=('' '')
+    return
+  fi
+
+  if [[ "${COMP_WORDS[$COMP_CWORD]}" == '?' ]]
+  then
+    # Include the actual runnable script
+    . "$ARGWORK_CLI_DIR/${__ARGWORK_SCRIPT_PATH}.sh"
+
+    __argwork_complete_help
+    return
+
+  elif [[ "${COMP_WORDS[$COMP_CWORD]}" == '??' && $COMP_TYPE == 63 ]]
+  then
+    # Include the actual runnable script
+    . "$ARGWORK_CLI_DIR/${__ARGWORK_SCRIPT_PATH}.sh"
+
+    __argwork_complete_inspect
+    return
+
+  elif [ "${COMP_CWORD}" -eq 1 ]
+  then
+    # The first section is completed with script names at the ARGWORK_CLI_DIR
+    # Start the script file name with a prefix '_ ' to make it available
+    # Sub-directories are supported and must start with the prefix as well
+    local IFS=$'\n'
+    local word="${COMP_WORDS[1]}"
+    local script_sub_rel="${word%/*}"
+    [[ "$word" != *'/'* ]] && script_sub_rel=
+    local script_sub_name="${word##*/}"
+    local script_sub_rel_dir="$(__argwork_script_name_to_path "${script_sub_rel}")"
+    local script_sub_abs_dir="$script_sub_rel_dir"
+    local script_dir="${ARGWORK_CLI_DIR}${script_sub_abs_dir}"
+
+    COMPREPLY=($(compgen -W "$(find "$script_dir" -mindepth 1 -maxdepth 1 -type f -name "_ ${script_sub_name}*" -printf "${script_sub_rel:+$script_sub_rel/}%f\n" -o -type d -name "_ ${script_sub_name}*" -printf "${script_sub_rel:+$script_sub_rel/}%f/\n" | sed 's/^_ //g; s/\/_ /\//g; s/.sh$//g'| sort)" -- "$word")) && compopt -o filenames
+    [[ $COMPREPLY == */ ]] && compopt -o nospace
+    return
+
+  elif [ "${COMP_CWORD}" -gt 1 ]
+  then
+      # Include the actual runnable script
+      . "$ARGWORK_CLI_DIR/${__ARGWORK_SCRIPT_PATH}.sh"
+
+      __argwork_complete
+    return
+  fi
+}
+
+# Wire up a custom command autocompletion in .bashrc:
+# complete -o nosort -F _argwork_completion <command
+
+
+# High level interface
+at() {
+  __argwork_one "$@"
+}
